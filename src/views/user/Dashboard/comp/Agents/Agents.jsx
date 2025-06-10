54002646445f1c39760af9ce7919aac8d8c3c131
kapilxenel <kapil.chandekar@xenelsoft.co.in>
2025-06-10 11:18:56 +0530

    added modal

--- src/views/user/Dashboard/comp/Agents/Agents.jsx
+++ src/views/user/Dashboard/comp/Agents/Agents.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import CustomTable from "@/components/custom/Table/Table";
 import { agentsColumns } from "@/constants/tableColumn";
 import { agentData } from "@/constants/tableData";
+import { DialogDemo } from "@/components/custom/Modal/Modal";
 
 const Agents = () => {
   return (
